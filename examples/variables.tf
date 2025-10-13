a7046684dbfed5e479a66d0192bc2ada82f99bd9
Filippo <filippo.merante@sparkfabrik.com>
2025-10-13 13:34:27 +0200

    fix example

--- examples/variables.tf
+++ examples/variables.tf
@@ -13,17 +13,17 @@ variable "notification_channels" {
 variable "kyverno" {
   description = "Configuration for Kyverno monitoring alerts. Allows customization of cluster name, project, notification channels, alert documentation, metric thresholds, auto-close timing, enablement, extra filters, and namespace."
   type = object({
-    enabled                 = optional(bool, true)
-    project_id              = optional(string, null)
-    cluster_name            = string
-    namespace               = optional(string, "kyverno")
-    notification_enabled    = optional(bool, true)
-    notification_channels   = optional(list(string), [])
-    alert_documentation     = optional(string, null)
-    metric_threshold_count  = optional(number, 2)
-    metric_lookback_minutes = optional(number, 1)
-    auto_close_seconds      = optional(number, 3600)
-    filter_extra            = optional(string, "")
+    enabled               = optional(bool, true)
+    cluster_name          = string
+    project_id            = optional(string, null)
+    notification_enabled  = optional(bool, true)
+    notification_channels = optional(list(string), [])
+    # Rate limit for notifications, e.g. "300s" for 5 minutes, used only for log match alerts
+    logmatch_notification_rate_limit = optional(string, "300s")
+    alert_documentation              = optional(string, null)
+    auto_close_seconds               = optional(number, 3600)
+    filter_extra                     = optional(string, "")
+    namespace                        = optional(string, "kyverno")
   })
 }
 
@@ -31,7 +31,7 @@ variable "cert_manager" {
   description = "Configuration for cert-manager missing issuer log alert. Allows customization of project, cluster, namespace, notification channels, alert documentation, enablement, extra filters, auto-close timing, and notification rate limiting."
   type = object({
     enabled                          = optional(bool, true)
-    cluster_name                     = optional(string, "")
+    cluster_name                     = string
     project_id                       = optional(string, null)
     namespace                        = optional(string, "cert-manager")
     notification_enabled             = optional(bool, true)
